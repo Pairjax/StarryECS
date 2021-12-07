2fa0edb822468f3dd5df5e29a1ebdca1863e7533
Austin Smith <pairjax@protonmail.com>
2021-12-07 12:19:11 -0500

    Add World Shell + Storage System
    Added a debug mode to the World as well as the Storage class, which
    stores all the Tables and tracks ID information.

--- src/main/kotlin/com/pairjax/starry/world/World.kt
+++ src/main/kotlin/com/pairjax/starry/world/World.kt
@@ -1,4 +1,15 @@
 package com.pairjax.starry.world
 
-class World {
+/**
+ * The World is the box that holds all ECS operations; you can have multiple worlds,
+ * but they will not interact unless you design specific Systems to interface between
+ * them.
+ */
+class World(name: String) {
+    val debug = true;
+    val storage: Store = Store(this);
+
+    init {
+        if (debug) print("[DEBUG] Loading World $name")
+    }
 }
