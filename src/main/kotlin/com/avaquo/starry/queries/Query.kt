466378af62bc6de8355660b413a553711bb86825
Austin Smith <pairjax@protonmail.com>
2021-12-20 10:38:52 -0500

    Queries
    - Added cached queries for readily accessible data

--- src/main/kotlin/com/avaquo/starry/queries/Query.kt
+++ src/main/kotlin/com/avaquo/starry/queries/Query.kt
@@ -1,13 +1,18 @@
 package com.avaquo.starry.queries
 
+import com.avaquo.starry.entities.Entity
 import com.avaquo.starry.ids.ID
 import com.avaquo.starry.ids.IDs
 import com.avaquo.starry.world.Store
 
 class Query(
-    store: Store,
-    terms: MutableList<Term>,
-    var cache: HashMap<ID, Any>
-): Filter(store, terms) {
+    val store: Store,
+    val filter: Filter,
+    var cache: HashMap<ID, Entity>
+) {
+    fun searchAndCache() {
+        val ids = filter.search()
 
+        for (id in ids) cache[id] = store.getEntity(id)
+    }
 }
--- src/main/kotlin/com/avaquo/starry/queries/Term.kt
+++ src/main/kotlin/com/avaquo/starry/queries/Term.kt
@@ -11,46 +11,8 @@ import com.avaquo.starry.world.Store
  * @param idType The type of Element being searched for
  * @param parameter (Optional) Specifies the data inside elements.
  */
-class Term(
+data class Term(
     private val store: Store,
     val idType: ID,
     val parameter: Any? = null
-) {
-
-    fun search(): IDs {
-        val foundElements: IDs = mutableListOf()
-
-        store.tables.map {
-            if (store.getEntity(it[0]).type.contains(idType)) {
-                foundElements += if (parameter != null) parseParameters(getElements(it))
-                                 else getElements(it)
-            }
-        }
-
-        return foundElements
-    }
-
-    private fun getElements(entities: Table): IDs {
-        val typeElements: IDs = mutableListOf()
-
-        entities.map { entityID ->
-            val e = store.getEntity(entityID)
-
-            e.ids.map { elementID ->
-                if (store.getElement(elementID)::class.simpleName == store.getElement(idType)) {
-                    typeElements += elementID
-                }
-            }
-        }
-
-        return typeElements
-    }
-
-    private fun parseParameters(elements: IDs): IDs {
-        val validElements: IDs = mutableListOf()
-
-        elements.map { if (store.getElement(it) == parameter) validElements += it }
-
-        return validElements
-    }
-}
+)
