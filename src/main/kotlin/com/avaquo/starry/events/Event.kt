c09a69d60f72851ef0c3205b674fb8723ecffe4b
Austin Smith <pairjax@protonmail.com>
2021-12-22 18:00:29 -0500

    Half-Events
    - Ok seriously how are events supposed to work????

--- src/main/kotlin/com/avaquo/starry/events/Event.kt
+++ src/main/kotlin/com/avaquo/starry/events/Event.kt
@@ -1,4 +1,23 @@
 package com.avaquo.starry.events
 
-class Event {
+import com.avaquo.starry.entities.Entity
+import com.avaquo.starry.ids.ID
+import com.avaquo.starry.ids.IDs
+import com.avaquo.starry.queries.Filter
+import com.avaquo.starry.queries.Query
+import com.avaquo.starry.systems.System
+import com.avaquo.starry.world.Store
+
+class Event(
+    name: String,
+    id: ID,
+    private val observer: Observer,
+    val filter: Filter,
+    private val callback: () -> Unit
+): Entity(name, id) {
+    fun runEvent(condition: ID) {
+        if (!observer.conditionMet(condition)) return
+
+        callback.invoke()
+    }
 }
--- src/main/kotlin/com/avaquo/starry/example/StarryCmd.kt
+++ src/main/kotlin/com/avaquo/starry/example/StarryCmd.kt
@@ -1,4 +1,11 @@
 package com.avaquo.starry.example
 
 class StarryCmd {
+    fun testCallbacks() {
+        val callback = {
+            fun testCallbackFunction() {
+                print("Hello!")
+            }
+        }
+    }
 }
